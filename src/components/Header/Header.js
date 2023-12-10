6f3de46cd6dbd7a53a541f0fb0cf1498910b62c0
onkarnora <onora155@gmail.com>
2023-12-10 11:07:21 +0530

    added info header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import './style.css'
 import EmailIcon from '@mui/icons-material/Email';
+import CallIcon from '@mui/icons-material/Call';
+import PersonIcon from '@mui/icons-material/Person';
 
 function Header() {
   return (
     <div className='header'>
         <div className='header-info'>
           <div className='contact-info'>
-            <EmailIcon />
+            <div className='d-flex-full'>
+              <EmailIcon /> <span className='p-6'>info@orientpolypack.com</span>
+              <CallIcon /> <span className='p-6'>+91 9923620479</span>
+            </div>
+            <div className='d-flex-full'>
+              <PersonIcon /> <span className='p-6'>User 1</span>
+            </div>
           </div>
         </div>
-        <div className='navbar'>Header</div>
+        <div className='navbar'></div>
     </div>
 
   )
